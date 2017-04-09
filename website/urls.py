0a9d65ace8388f5148bfcfeba5d66db3e42101d3
helemork <helemork@gmail.com>
2017-04-09 19:53:37 +0200

    links on the links

--- website/urls.py
+++ website/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^songs/$', views.songs, name='songs'),
+    url(r'^vote/([0-9]+)/$', views.vote, name='vote'),
     url(r'^scoreboard/$', views.scoreboard, name='scoreboard'),
     url(r'^registration/$', views.registration, name='registration'),
     url(r'^login/$', views.login, name='login'),
