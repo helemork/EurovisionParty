0cc5487446a7bdb47f75781358d7608c838d359a
Erik Smistad <ersmistad@gmail.com>
2017-04-09 17:21:54 +0200

    Login and registration

--- website/views.py
+++ website/views.py
@@ -1,11 +1,34 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request)
+        if form.is_valid():
+            return redirect('index')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'login.html', {
+        'form': form
+    })
+
 def registration(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user) # This will login the user
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+
     return render(request, 'registration.html', {
-        'form': UserCreationForm()
+        'form': form
     })
