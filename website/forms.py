d7a20dea22fd5c2f2285ff22302f330a66ccf6fa
helemork <helemork@gmail.com>
2017-04-10 02:25:18 +0200

    fixed the bug with the forms (added missing fields), but discovered another: impossible to change votes for a country

--- website/forms.py
+++ website/forms.py
@@ -4,6 +4,8 @@ from .models import *
 class VoteForm(forms.ModelForm):
     class Meta:
         model = Vote
-        fields = ['show', 'lightshow', 'outfit', 'melody', 'voice', 'esc_factor', 'sexyness',
-                  'douchebag', 'modulation', 'dress_change', 'language']
-
+        fields = ['show', 'lightshow',
+                  'outfit', 'dress','melody',
+                  'voice', 'esc_factor', 'sexyness',
+                  'douchebag', 'modulation',
+                  'dress_change', 'language','plagiarism']
