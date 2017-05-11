3492abcdab98f2acf1e95e742f567cc63fc1d59c
Erik Smistad <ersmistad@gmail.com>
2017-05-11 20:22:54 +0200

    Fixed bug with total score not including artbreak

--- website/models.py
+++ website/models.py
@@ -39,4 +39,4 @@ class Vote(models.Model):
     def get_score(self):
         return self.show + self.lightshow + self.outfit + self.melody + \
                 self.voice + self.esc_factor + self.sexyness + self.douchebag + self.modulation + \
-                self.dress_change + self.language
+                self.dress_change + self.language + self.artbreak
